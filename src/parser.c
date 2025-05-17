7f07279d0d94bc072fbe6e047b86bcb87dbc4217
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-16 18:06:58 -0700

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,7 @@
+#include "parser.h"
+#include "graph.h"
 
+// Stub: Parses the given markdown SRS file and extracts requirements
+void parse_srs(const char *filename) {
+    // TODO: Implement parsing logic.
+}
