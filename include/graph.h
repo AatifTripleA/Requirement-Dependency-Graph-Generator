3688a7eb83dfe498b7a6ec4d5acf3c6cb59c27b0
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-16 18:06:01 -0700

    Update graph.h

--- include/graph.h
+++ include/graph.h
@@ -1,1 +1,13 @@
+#ifndef GRAPH_H
+#define GRAPH_H
 
+// Initializes the requirement dependency graph
+void graph_init(void);
+
+// Adds a dependency between two requirements
+void graph_add_dependency(const char *from, const char *to);
+
+// Frees resources used by the graph
+void graph_free(void);
+
+#endif
