daeba1479d60124102080f7882a386498814155e
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-16 18:07:30 -0700

    Update report.c

--- src/report.c
+++ src/report.c
@@ -1,1 +1,7 @@
+#include "report.h"
+#include "graph.h"
 
+// Stub: Generates a report of the requirement dependency graph.
+void generate_report(const char *filename) {
+    // TODO: Implement report generation.
+}
