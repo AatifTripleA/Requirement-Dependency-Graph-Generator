cf66e19e8bd0ad05d08c139a6bdfc7540669f562
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-19 19:36:25 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,12 +4,14 @@
 #include "graph.h"
 
 int main() {
-    void get_requirement(const char *file_name);
-    void parse_srs(const char *file_name);
-    void print_parse(void);
-    void graph_generator(const char *file_name);
-    void print_graph(const char *file_name);
-    void report_creation(const char *dependency_req);
+    // void get_requirement(const char *file_name);
+    // void parse_srs(const char *file_name);
+    // void print_parse(void);
+    // void graph_generator(const char *file_name);
+    // void print_graph(const char *file_name);
+    // void report_creation(const char *dependency_req);
+    
+    // Stubs
     
     return 0;
 }
