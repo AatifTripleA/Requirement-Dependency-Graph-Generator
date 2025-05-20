3e063d882e3fa3a7ee2f751db292ded55a7be54d
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-19 18:15:35 -0700

    Update graph.c

--- src/graph.c
+++ src/graph.c
@@ -1,16 +1,15 @@
+#include <stdio.h>
+#include "parse.h"
 #include "graph.h"
 
-// Stub: Initializes the requirement dependency graph.
-void graph_init(void) {
-    // TODO: Implement graph initialization.
+// Code to be implemented
+
+// Get requirement
+void get_requirement(const char *file_name) {
+    // Stub
 }
 
-// Stub: Adds a dependency between two requirements.
-void graph_add_dependency(const char *from, const char *to) {
-    // TODO: Implement adding dependency.
-}
-
-// Stub: Frees resources used by the graph.
-void graph_free(void) {
-    // TODO: Implement resource cleanup.
+// Generate the graph
+void graph_generator(const char *file_name) {
+    // Stub
 }
