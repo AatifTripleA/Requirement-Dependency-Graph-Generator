4d6268e2d263a0ee0efc8064c8ffef3db3595b78
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-19 18:35:17 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include "parse.h"
-#include "graph.h"
 #include "report.h"
+#include "graph.h"
 
-int main(int argc, char *argv[]) {
-    // TODO: Implement main program logic.
+int main() {
+    void parse_srs(const char *file_name);
+    void graph_generator(const char *file_name);
+    void print_graph(const char *file_name);
+    void report_creation(const char *dependency_req);
     return 0;
 }
