942cdb77c12826bc65039c9d8ab2f32e440af4a9
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-19 19:00:20 -0700

    Update parse.c

--- src/parse.c
+++ src/parse.c
@@ -4,8 +4,10 @@
 
 void parse_srs(const char *file_name) {
     // Stub
+    // The logic for the parsing of the file - will note down important details like dependency information
 }
 
 void print_parse(void) {
     // Stub
+    // Prints the requirements and dependencies
 }
