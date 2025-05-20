70b2947d8aabb9ddf6471318a22b7129ae44c79f
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-19 18:29:37 -0700

    Update graph.c

--- src/graph.c
+++ src/graph.c
@@ -4,12 +4,16 @@
 
 // Code to be implemented
 
-// Get requirement
+// Get requirements for dependencies from the markdown file
 void get_requirement(const char *file_name) {
     // Stub
 }
 
 // Generate the graph
 void graph_generator(const char *file_name) {
+    // Stub
+}
+
+void print_graph(const char *file_name) {
     // Stub
 }
