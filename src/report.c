afadc92597734afea820e1fe188d983621a623e3
Aatif Ahmed <mr.aatifahmed@gmail.com>
2025-05-27 20:40:59 -0700

    Update report.c

--- src/report.c
+++ src/report.c
@@ -2,7 +2,27 @@
 #include "report.h"
 #include "graph.h"
 
-void report_creation(const char *dependency_req) {
-    // Stub
-    // A report will be created based on the requirement dependencies given
+extern Requirement requirements[];
+extern int requirement_count;
+
+void report_creation(const char *output_file) {
+    FILE *file = fopen(output_file, "w");
+    if (!file) {
+        printf("Error: Could not create report file %s\n", output_file);
+        return;
+    }
+
+    fprintf(file, "# <center>Software Requirements Specification </center>\n");
+    fprintf(file, "<center> EECS 22L - Spring 2025 </center>\n");
+    fprintf(file, "<center> Project 1 - Chess Game </center>\n\n");
+
+    for (int i = 0; i < requirement_count; i++) {
+        fprintf(file, "Line %d: %s --\n", i + 1, requirements[i].id);
+        for (int j = 0; j < requirements[i].child_count; j++) {
+            fprintf(file, "Line %d: %s -> %s\n", i + 1, requirements[i].id, requirements[i].children[j]);
+        }
+    }
+
+    fclose(file);
+    printf("Report created: %s\n", output_file);
 }
